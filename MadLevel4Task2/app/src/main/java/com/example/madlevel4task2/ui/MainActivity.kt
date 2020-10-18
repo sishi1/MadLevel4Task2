b2f0b0c283559b8421a836f1fa3cabe90f7b589c
Sishi <sishi_hu12@hotmail.com>
2020-10-18 23:03:45 +0200

    Changed layout and edited class names

--- MadLevel4Task2/app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
+++ MadLevel4Task2/app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
@@ -1,22 +1,40 @@
-package com.example.madlevel4task2
+package com.example.madlevel4task2.ui
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.madlevel4task2.R
+import com.example.madlevel4task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        navController = findNavController(R.id.nav_host_fragment)
+
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        fabToggler()
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.FirstFragment))
+                binding.fab.show()
+            else
+                binding.fab.hide()
         }
     }
 
