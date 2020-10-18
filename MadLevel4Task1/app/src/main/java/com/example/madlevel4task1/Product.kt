e02ba0db6d8b3dec511158a85b24bc36cc4796cf
Sishi <sishi_hu12@hotmail.com>
2020-10-18 16:00:25 +0200

    Changed product class and small stuff in gradle

--- MadLevel4Task1/app/src/main/java/com/example/madlevel4task1/Product.kt
+++ MadLevel4Task1/app/src/main/java/com/example/madlevel4task1/Product.kt
@@ -1,6 +1,18 @@
 package com.example.madlevel4task1
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "productTable")
 data class Product (
+    @ColumnInfo(name = "product")
     var productName: String,
-    var productQuantity: Int
+
+    @ColumnInfo(name = "quantity")
+    var productQuantity: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
 )
